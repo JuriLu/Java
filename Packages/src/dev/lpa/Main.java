3007988f4b72ac8510a1c42d0d74b4ef1b458e9a
jlubonja <lubonjajurgen1@gmail.com>
2024-12-22 19:33:21 +0100

    Packages

---
+++ Packages/src/dev/lpa/Main.java
@@ -0,0 +1,11 @@
+package dev.lpa;
+
+
+import com.abc.first.Item;
+
+public class Main {
+    public static void main(String[] args) {
+        Item firstItem = new Item("First Item");
+        System.out.println(firstItem);
+    }
+}
